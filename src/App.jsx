58f7ca00414f5ffee1e5a68cfaae5e24cc02371e
Jasper Van Dyck <jasper.vandyck@student.hogent.be>
2023-05-01 20:24:19 +0200

    added main Parallax component

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ function App() {
   return (
     <>
       <div>
-        <Parallax pages={4}>
+      <Parallax pages={4} ref={ref}>
           <ParallaxLayer></ParallaxLayer>
         </Parallax>
       </div>
+      </Parallax>
     </>
   );
 }
