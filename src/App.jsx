5161b09cce5d9361a82e6a4cd503bc4e0f1d8280
Jasper Van Dyck <jasper.vandyck@student.hogent.be>
2023-05-01 20:25:07 +0200

    added parallax backroungd

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,29 @@ import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 function App() {
   return (
-    <>
-      <div>
+    <div>
       <Parallax pages={4} ref={ref}>
-          <ParallaxLayer></ParallaxLayer>
-        </Parallax>
-      </div>
+        <ParallaxLayer
+          offset={0}
+          speed={1}
+          factor={2}
+          style={{
+            backgroundImage: `url(${landscape})`,
+            backgroundSize: "cover",
+          }}
+        />
+
+        <ParallaxLayer
+          offset={2}
+          speed={1}
+          factor={4}
+          style={{
+            backgroundImage: `url(${cave})`,
+            backgroundSize: "cover",
+          }}
+        />
       </Parallax>
-    </>
+    </div>
   );
 }
 
